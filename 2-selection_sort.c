94265915033adad8f42ef444a537d02de4ae60bb
oumaima <elghachiomaima1@gmail.com>
2023-11-17 12:06:20 +0100

    test

---
+++ 2-selection_sort.c
@@ -0,0 +1,14 @@
+/*
+ * File: 2-selection_sort.c
+ *
+ */
+
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
