dd4dbc9bf2516e4950fc1c92f2f8ad84a818224a
oumaima <elghachiomaima1@gmail.com>
2023-11-17 12:08:39 +0100

    test

---
+++ deck.h
@@ -0,0 +1,19 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <stdlib.h>
+
+/**
+ * enum kind_e - Enumeration of card suits.
+ * @SPADE: Spades suit.
+ * @HEART: Hearts suit.
+ * @CLUB: Clubs suit.
+ * @DIAMOND: Diamonds suit.
+ */
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
