ebd46d77ede673b186aa2f4f2fa106dda2711385
oumaima <elghachiomaima1@gmail.com>
2023-11-17 12:07:44 +0100

    test

---
+++ 3-quick_sort.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-quick_sort.c
+ */
+
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
