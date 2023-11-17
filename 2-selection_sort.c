2f9c55dc916c1bbc4be105c984b5b7d652db2be6
oumaima <elghachiomaima1@gmail.com>
2023-11-17 12:10:34 +0100

    test1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,3 +12,20 @@
  */
 void swap_ints(int *a, int *b)
 {
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
