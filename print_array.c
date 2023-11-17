641216fc0f6da8de6ff852d2271443b71a3cbcb9
oumaima <elghachiomaima1@gmail.com>
2023-11-17 12:19:03 +0100

    test2

---
+++ print_array.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
